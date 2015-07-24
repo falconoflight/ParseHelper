e56b9b19376185547cd03f8827b3fd0a6e5cc9b6
Vasu Laroiya <vasularoiya@Vasus-MacBook-Pro.local>
2015-07-24 01:40:51 -0400

    Renamed some functions

--- ParseHelper/ParseLogic.swift
+++ ParseHelper/ParseLogic.swift
@@ -17,7 +17,7 @@ class ParseLogic: NSObject {
     Logs the user in
     */
     
-    func loginUser(email:String, password:String, completion:((success:Bool, errorMesssage:String?)->Void)?) {
+    func logInUser(email:String, password:String, completion:((success:Bool, errorMesssage:String?)->Void)?) {
         PFUser.logInWithUsernameInBackground(email.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet()), password: password, block: { (user:PFUser?, error:NSError?) -> Void in
             if error == nil {
                 if completion != nil {
@@ -35,10 +35,16 @@ class ParseLogic: NSObject {
     Creates a new user
     */
     
-    func createUser(fullName:String?, email:String, password:String, confirmPassword:String?, completion:((success:Bool, error:String?)->Void)?) {
+    func createUser(email:String, password:String, confirmPassword:String?, parameters:[String:AnyObject]?, completion:((success:Bool, errorMesssage:String?)->Void)?) {
         let user = PFUser()
         user.email = email.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
         user.username = user.email
+        
+        if parameters != nil {
+            for parameter in parameters! {
+                user[parameter.0] = parameter.1
+            }
+        }
         
         if confirmPassword != nil {
             
@@ -50,25 +56,25 @@ class ParseLogic: NSObject {
                     user.signUpInBackgroundWithBlock({ (success, error) -> Void in
                         if error == nil {
                             if completion != nil {
-                                completion!(success: true, error: nil)
+                                completion!(success: true, errorMesssage: nil)
                             }
                         } else {
                             if completion != nil {
                                 var errorMessage = getErrorMessage(error!)
                                 let email = email.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
                                 errorMessage = errorMessage.stringByReplacingOccurrencesOfString(" \(email)", withString: "")
-                                completion!(success: false, error: errorMessage)
+                                completion!(success: false, errorMesssage: errorMessage)
                             }
                         }
                     })
                 } else {
                     if completion != nil {
-                        completion!(success: false, error: "Passwords do not match")
+                        completion!(success: false, errorMesssage: "Passwords do not match")
                     }
                 }
             } else {
                 if completion != nil {
-                    completion!(success: false, error: "Enter a valid password")
+                    completion!(success: false, errorMesssage: "Enter a valid password")
                 }
             }
         } else {
@@ -77,14 +83,14 @@ class ParseLogic: NSObject {
             user.signUpInBackgroundWithBlock({ (success, error) -> Void in
                 if error == nil {
                     if completion != nil {
-                        completion!(success: true, error: nil)
+                        completion!(success: true, errorMesssage: nil)
                     }
                 } else {
                     if completion != nil {
                         var errorMessage = getErrorMessage(error!)
                         let email = email.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
                         errorMessage = errorMessage.stringByReplacingOccurrencesOfString(" \(email)", withString: "")
-                        completion!(success: false, error: errorMessage)
+                        completion!(success: false, errorMesssage: errorMessage)
                     }
                 }
             })
@@ -95,16 +101,16 @@ class ParseLogic: NSObject {
     Sends password reset email
     */
     
-    func userForgotPassword(var email:String, completion:((success:Bool, error:String?)->Void)?) {
+    func sendForgotPasswordEmail(var email:String, completion:((success:Bool, errorMesssage:String?)->Void)?) {
         email = email.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
         PFUser.requestPasswordResetForEmailInBackground(email) { (success, error) -> Void in
             if error == nil {
                 if completion != nil {
-                    completion!(success: true, error: nil)
+                    completion!(success: true, errorMesssage: nil)
                 }
             } else {
                 if completion != nil {
-                    completion!(success: false, error: getErrorMessage(error!).stringByReplacingOccurrencesOfString("with email \(email)", withString: "with this email"))
+                    completion!(success: false, errorMesssage: getErrorMessage(error!).stringByReplacingOccurrencesOfString("with email \(email)", withString: "with this email"))
                 }
             }
         }
@@ -114,16 +120,16 @@ class ParseLogic: NSObject {
     Login user using Facebook with read permissions
     */
     
-    func loginUserUsingFacebookReadPermissions(permissions:[AnyObject]?, completion:((success:Bool, error:String?)->Void)?) {
+    func loginUserUsingFacebookReadPermissions(permissions:[AnyObject]?, completion:((success:Bool, errorMesssage:String?)->Void)?) {
         PFFacebookUtils.logInInBackgroundWithReadPermissions(permissions) {
             (user: PFUser?, error: NSError?) -> Void in
             if error == nil {
                 if completion != nil {
-                    completion!(success: true, error: nil)
+                    completion!(success: true, errorMesssage: nil)
                 }
             } else {
                 if completion != nil {
-                    completion!(success: false, error: getErrorMessage(error!))
+                    completion!(success: false, errorMesssage: getErrorMessage(error!))
                 }
             }
         }
@@ -133,16 +139,16 @@ class ParseLogic: NSObject {
     Login user using Facebook with publish permissions
     */
     
-    func loginUserUsingFacebookPublishPermissions(permissions:[AnyObject]?, completion:((success:Bool, error:String?)->Void)?) {
+    func loginUserUsingFacebookPublishPermissions(permissions:[AnyObject]?, completion:((success:Bool, errorMesssage:String?)->Void)?) {
         PFFacebookUtils.logInInBackgroundWithPublishPermissions(permissions) {
             (user: PFUser?, error: NSError?) -> Void in
             if error == nil {
                 if completion != nil {
-                    completion!(success: true, error: nil)
+                    completion!(success: true, errorMesssage: nil)
                 }
             } else {
                 if completion != nil {
-                    completion!(success: false, error: getErrorMessage(error!))
+                    completion!(success: false, errorMesssage: getErrorMessage(error!))
                 }
             }
         }
@@ -152,17 +158,17 @@ class ParseLogic: NSObject {
     Link user to Facebook with read permissions
     */
     
-    func linkFacebookToUserUsingReadPermissions(permissions:[AnyObject]?, completion:((success:Bool, error:String?)->Void)?) {
+    func linkFacebookToUserUsingReadPermissions(permissions:[AnyObject]?, completion:((success:Bool, errorMesssage:String?)->Void)?) {
         if let user = PFUser.currentUser() {
             if !PFFacebookUtils.isLinkedWithUser(user) {
                 PFFacebookUtils.linkUserInBackground(user, withReadPermissions: permissions, block: { (success, error) -> Void in
                     if error == nil {
                         if completion != nil {
-                            completion!(success: true, error: nil)
+                            completion!(success: true, errorMesssage: nil)
                         }
                     } else {
                         if completion != nil {
-                            completion!(success: false, error: getErrorMessage(error!))
+                            completion!(success: false, errorMesssage: getErrorMessage(error!))
                         }
                     }
                 })
@@ -174,17 +180,17 @@ class ParseLogic: NSObject {
     Link user to Facebook with publish permissions
     */
     
-    func linkFacebookToUserUsingPublishPermissions(permissions:[AnyObject], completion:((success:Bool, error:String?)->Void)?) {
+    func linkFacebookToUserUsingPublishPermissions(permissions:[AnyObject], completion:((success:Bool, errorMesssage:String?)->Void)?) {
         if let user = PFUser.currentUser() {
             if !PFFacebookUtils.isLinkedWithUser(user) {
                 PFFacebookUtils.linkUserInBackground(user, withPublishPermissions: permissions, block: { (success, error) -> Void in
                     if error == nil {
                         if completion != nil {
-                            completion!(success: true, error: nil)
+                            completion!(success: true, errorMesssage: nil)
                         }
                     } else {
                         if completion != nil {
-                            completion!(success: false, error: getErrorMessage(error!))
+                            completion!(success: false, errorMesssage: getErrorMessage(error!))
                         }
                     }
                 })
@@ -196,15 +202,15 @@ class ParseLogic: NSObject {
     Login user using Twitter
     */
     
-    func loginUserUsingTwitter(completion:((success:Bool, error:String?)->Void)?) {
+    func loginUserUsingTwitter(completion:((success:Bool, errorMesssage:String?)->Void)?) {
         PFTwitterUtils.logInWithBlock { (user, error) -> Void in
             if error == nil {
                 if completion != nil {
-                    completion!(success: true, error: nil)
+                    completion!(success: true, errorMesssage: nil)
                 }
             } else {
                 if completion != nil {
-                    completion!(success: false, error: getErrorMessage(error!))
+                    completion!(success: false, errorMesssage: getErrorMessage(error!))
                 }
             }
         }
@@ -214,16 +220,16 @@ class ParseLogic: NSObject {
     Link user using Twitter
     */
     
-    func linkTwitterToUser(completion:((success:Bool, error:String?)->Void)?) {
+    func linkTwitterToUser(completion:((success:Bool, errorMesssage:String?)->Void)?) {
         if let user = PFUser.currentUser() {
             PFTwitterUtils.linkUser(user, block: { (success, error) -> Void in
                 if error == nil {
                     if completion != nil {
-                        completion!(success: true, error: nil)
+                        completion!(success: true, errorMesssage: nil)
                     }
                 } else {
                     if completion != nil {
-                        completion!(success: false, error: getErrorMessage(error!))
+                        completion!(success: false, errorMesssage: getErrorMessage(error!))
                     }
                 }
             })
@@ -234,7 +240,7 @@ class ParseLogic: NSObject {
     Saves object to a specific class on Parse server
     */
     
-    func saveObject(className:String, parameters:[String:AnyObject]?, completion:((success:Bool, error:String?)->Void)?) {
+    func saveObject(className:String, parameters:[String:AnyObject]?, completion:((success:Bool, errorMesssage:String?)->Void)?) {
         let objectClass = PFObject(className: className)
 
         if parameters != nil {
@@ -246,11 +252,11 @@ class ParseLogic: NSObject {
         objectClass.saveInBackgroundWithBlock({ (success, error) -> Void in
             if error == nil {
                 if completion != nil {
-                    completion!(success: true, error: nil)
+                    completion!(success: true, errorMesssage: nil)
                 }
             } else {
                 if completion != nil {
-                    completion!(success: false, error: getErrorMessage(error))
+                    completion!(success: false, errorMesssage: getErrorMessage(error))
                 }
             }
         })
@@ -260,7 +266,7 @@ class ParseLogic: NSObject {
     Retrieves objects from a specific class on Parse server
     */
     
-    func retrievesObject(className:String, objectID:String?, parameters:[String:AnyObject]?, completion:(success:Bool, error:String?, object:[AnyObject]?)->Void) {
+    func retrievesObject(className:String, objectID:String?, parameters:[String:AnyObject]?, completion:(success:Bool, errorMesssage:String?, object:[AnyObject]?)->Void) {
         let query = PFQuery(className:className)
         
         if parameters != nil {
@@ -272,17 +278,17 @@ class ParseLogic: NSObject {
         if objectID != nil {
             query.getObjectInBackgroundWithId(objectID!, block: { (object, error) -> Void in
                 if error == nil {
-                    completion(success: true, error: nil, object: [object as! AnyObject])
+                    completion(success: true, errorMesssage: nil, object: [object as! AnyObject])
                 } else {
-                    completion(success: false, error: getErrorMessage(error), object: nil)
+                    completion(success: false, errorMesssage: getErrorMessage(error), object: nil)
                 }
             })
         } else {
             query.findObjectsInBackgroundWithBlock({ (objects, error) -> Void in
                 if error == nil {
-                    completion(success: true, error: nil, object: objects)
+                    completion(success: true, errorMesssage: nil, object: objects)
                 } else {
-                    completion(success: false, error: getErrorMessage(error), object: nil)
+                    completion(success: false, errorMesssage: getErrorMessage(error), object: nil)
                 }
 
             })
@@ -293,7 +299,7 @@ class ParseLogic: NSObject {
     Saves a object to local data storage
     */
     
-    func saveObjectToLocalDataStorage(className:String, parameters:[String:AnyObject], pinName:String, completion:((success:Bool, error:String?)->Void)?) {
+    func saveObjectToLocalDataStorage(className:String, parameters:[String:AnyObject], pinName:String, completion:((success:Bool, errorMesssage:String?)->Void)?) {
         let objectClass = PFObject(className: className)
 
         for parameter in parameters {
@@ -304,11 +310,11 @@ class ParseLogic: NSObject {
         objectClass.pinInBackgroundWithName(pinName, block: { (success, error) -> Void in
             if error == nil {
                 if completion != nil {
-                    completion!(success: true, error: nil)
+                    completion!(success: true, errorMesssage: nil)
                 }
             } else {
                 if completion != nil {
-                    completion!(success: false, error: getErrorMessage(error))
+                    completion!(success: false, errorMesssage: getErrorMessage(error))
                 }
             }
         })
@@ -318,14 +324,14 @@ class ParseLogic: NSObject {
     Retrieves objects from local data storage
     */
     
-    func retrieveObjectsFromLocalDataStorage(className:String, pinName:String, completion:(success:Bool, error:String?, result:[AnyObject]?)->Void) {
+    func retrieveObjectsFromLocalDataStorage(className:String, pinName:String, completion:(success:Bool, errorMesssage:String?, result:[AnyObject]?)->Void) {
         let query = PFQuery(className: className)
         query.fromPinWithName(pinName)
         query.findObjectsInBackgroundWithBlock { (objects, error) -> Void in
             if error == nil {
-                completion(success: true, error: nil, result: objects)
+                completion(success: true, errorMesssage: nil, result: objects)
             } else {
-                completion(success: false, error: getErrorMessage(error), result: nil)
+                completion(success: false, errorMesssage: getErrorMessage(error), result: nil)
             }
         }
     }
@@ -334,15 +340,15 @@ class ParseLogic: NSObject {
     Deletes object from local data storage
     */
     
-    func unpinObject(pinName:String, completion:((success:Bool, error:String?)->Void)?) {
+    func unpinObject(pinName:String, completion:((success:Bool, errorMesssage:String?)->Void)?) {
         PFObject.unpinAllObjectsInBackgroundWithName(pinName, block: { (success, error) -> Void in
             if error == nil {
                 if completion != nil {
-                    completion!(success: true, error: nil)
+                    completion!(success: true, errorMesssage: nil)
                 }
             } else {
                 if completion != nil {
-                    completion!(success: false, error: getErrorMessage(error))
+                    completion!(success: false, errorMesssage: getErrorMessage(error))
                 }
             }
         })
@@ -352,16 +358,16 @@ class ParseLogic: NSObject {
     Deletes object from Parse server
     */
     
-    func deleteObject(object:PFObject, parameters:[String]?, completion:((success:Bool, error:String?)->Void)?) {
+    func deleteObject(object:PFObject, parameters:[String]?, completion:((success:Bool, errorMesssage:String?)->Void)?) {
         if parameters == nil {
             object.deleteInBackgroundWithBlock { (success, error) -> Void in
                 if error == nil {
                     if completion != nil {
-                        completion!(success: true, error: nil)
+                        completion!(success: true, errorMesssage: nil)
                     }
                 } else {
                     if completion != nil {
-                        completion!(success: false, error: getErrorMessage(error))
+                        completion!(success: false, errorMesssage: getErrorMessage(error))
                     }
                 }
             }
@@ -373,11 +379,11 @@ class ParseLogic: NSObject {
             object.saveInBackgroundWithBlock({ (success, error) -> Void in
                 if error == nil {
                     if completion != nil {
-                        completion!(success: true, error: nil)
+                        completion!(success: true, errorMesssage: nil)
                     }
                 } else {
                     if completion != nil {
-                        completion!(success: false, error: getErrorMessage(error))
+                        completion!(success: false, errorMesssage: getErrorMessage(error))
                     }
                 }
             })
--- ParseHelper/ViewController.swift
+++ ParseHelper/ViewController.swift
@@ -16,6 +16,33 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        let parseLogic = ParseLogic.sharedInstance
+        parseLogic.createUser("example@example.com", password: "password", confirmPassword: "password", parameters: ["fullName": "Example Example"]) { (success, errorMesssage) -> Void in
+            if errorMesssage == nil {
+                // User Created Successfully
+            } else {
+                // Error Occured
+                println(errorMesssage)
+            }
+        }
+        
+        parseLogic.logInUser("example@example.com", password: "password") { (success, errorMesssage) -> Void in
+            if success == false {
+                // User Logged in Successfully
+            } else {
+                // Error Occured
+                println(errorMesssage)
+            }
+        }
+        
+        parseLogic.sendForgotPasswordEmail("example@example.com", completion: { (success, errorMesssage) -> Void in
+            if success == false {
+                // User Logged in Successfully
+            } else {
+                // Error Occured
+                println(errorMesssage)
+            }
+        })
     }
     
     override func didReceiveMemoryWarning() {
